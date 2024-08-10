de27b213b53c4b243f69f72ec487ce479f72eddf
max. <max@someone.ky>
2024-08-10 20:57:11 +0200

    Stop using method references for item registry
    turns out, they *do* load the target class!

--- src/main/java/ky/someone/mods/framingtemplates/item/FramingItems.java
+++ src/main/java/ky/someone/mods/framingtemplates/item/FramingItems.java
@@ -9,11 +9,13 @@ import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Function;
 
+// oh you're kidding, THAT is the issue? sometimes, you hurt me, java...
+@SuppressWarnings("Convert2MethodRef")
 public interface FramingItems {
 	DeferredRegister<Item> REGISTRY = DeferredRegister.create(Registries.ITEM, FramingUtil.MOD_ID);
 
-	RegistryObject<FramingTemplateItem> FUNCTIONAL_STORAGE_TEMPLATE = forMod("functional_storage_template", "functionalstorage", FunctionalStorageTemplate::new);
-	RegistryObject<FramingTemplateItem> STORAGE_DRAWERS_TEMPLATE = forMod("storage_drawers_template", "framedcompactdrawers", StorageDrawersTemplate::new);
+	RegistryObject<FramingTemplateItem> FUNCTIONAL_STORAGE_TEMPLATE = forMod("functional_storage_template", "functionalstorage", modid -> new FunctionalStorageTemplate(modid));
+	RegistryObject<FramingTemplateItem> STORAGE_DRAWERS_TEMPLATE = forMod("storage_drawers_template", "framedcompactdrawers", modid -> new StorageDrawersTemplate(modid));
 
 	static <T extends FramingTemplateItem> RegistryObject<T> forMod(String id, String modid, Function<String, T> factory) {
 		if (ModList.get().isLoaded(modid)) {
